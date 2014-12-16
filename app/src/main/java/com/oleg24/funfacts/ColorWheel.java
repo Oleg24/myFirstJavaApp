d6bf62cc77e421f45c6b7983e8bbb86339210f56
Oleg <o.yanchinskiy@gmail.com>
2014-12-16 13:45:47 -0800

    random colors being generated as background

---
+++ app/src/main/java/com/oleg24/funfacts/ColorWheel.java
@@ -0,0 +1,41 @@
+package com.oleg24.funfacts;
+
+import java.util.Random;
+import android.graphics.Color;
+
+/**
+ * Created by oleg on 12/16/14.
+ */
+public class ColorWheel {
+
+    public String[] mColor = {
+            "#39add1", // light blue
+            "#3079ab", // dark blue
+            "#c25975", // mauve
+            "#e15258", // red
+            "#f9845b", // orange
+            "#838cc7", // lavender
+            "#7d669e", // purple
+            "#53bbb4", // aqua
+            "#51b46d", // green
+            "#e0ab18", // mustard
+            "#637a91", // dark gray
+            "#f092b0", // pink
+            "#b7c0c7"  // light gray
+    };
+
+
+    public int getColor(){
+
+        String color = "";
+        // Randomly select a fact
+        Random randomGenerator = new Random();
+
+        int randomNumber = randomGenerator.nextInt(mColor.length);
+
+        color = mColor[randomNumber];
+        int colorAsInt = Color.parseColor(color);
+
+        return colorAsInt;
+    }
+}
--- app/src/main/java/com/oleg24/funfacts/FunFactActivity.java
+++ app/src/main/java/com/oleg24/funfacts/FunFactActivity.java
@@ -14,6 +14,7 @@ import java.util.Random;
 public class FunFactActivity extends ActionBarActivity {
 
     private FactBook mFactBook = new FactBook();
+    private ColorWheel mColorWheel = new ColorWheel();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +32,9 @@ public class FunFactActivity extends ActionBarActivity {
             @Override
             public void onClick(View view){
                 String fact = mFactBook.getFact();
-                relativeLayout.setBackgroundColor(Color.RED);
+
+                int color = mColorWheel.getColor();
+                relativeLayout.setBackgroundColor(color);
                 factLabel.setText(fact);
             }
         };
